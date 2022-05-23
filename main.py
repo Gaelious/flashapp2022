a0c89168fe4834a7d214986e2dfde268750a6054
RoachLok <jtabordajunior@hotmail.com>
2022-05-23 20:01:07 +0200

    Add getAdults and read agenda.
    By: Aimarcb <Aimarcb@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,43 +1,55 @@
 from flask import Flask, abort, jsonify, render_template
  
 app = Flask(__name__)
- 
+
+def readAgenda():
+    agenda = []
+
+    filepath = "data/agenda.csv"
+    with open(filepath, 'r') as fp:
+        line = fp.readline()
+        while line:
+            line = fp.readline()
+            if (line):
+                agenda.append(line.split(',',4))
+
+    return agenda
+
+agenda = readAgenda()
+
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
  
 @app.route("/text_simi")
 def text_simi():
-    agenda = []
- 
-    filepath = "data/agenda.csv"
-    with open(filepath, 'r') as fp:
-        line = fp.readline()
-        while line:
-            line = fp.readline()
-            agenda.append(line.split(',', 2))
-
     return render_template("text_simi.html", test=agenda)
 
 @app.route("/api/get_user/<user>")
 def getUser (user):
-    
-    agenda = []
-    filepath = "data/agenda.csv"
-    with open(filepath, 'r') as fp:
-        line = fp.readline()
-        while line:
-            line = fp.readline()
-            agenda.append(line.split(',', 2))
-
     for contact in agenda:
         if user == contact[0]:
-            return jsonify(name=contact[0], number=contact[1])
+            return jsonify(name=contact[0], number=contact[1], birth=contact[2], gender=contact[3])
         
 
     return jsonify(user=user, error="User not found."), 404
 
+@app.route("/api/get_adults")
+def getAdult():
 
+    adults = []
+
+    for contact in agenda:
+        year = int(contact[2][:4])
+        
+        if year < 2004:
+            adults.append([contact[0], contact[2]])
+    
+    return jsonify(adults)
+    
+            
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
